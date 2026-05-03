107bbb459d107762c72bd4eab58eaab8a499c0cc
John-Woodj <xasa123321@gmail.com>
2026-05-03 03:29:29 +0300

    1.0.0
    Чистка РХСа

---
+++ Scripts/Game/F_AttachmentSlotPLUS.c
@@ -0,0 +1,8 @@
+// scripts/Game/FORTEX_AttachmentSlotAK.c
+// Типы слотов для АК-семейства.
+// Головные типы — для слотов на оружии.
+// Составные (_1/_2/_3) — для внутренних слотов самого аттачмента.
+
+class FORTEX_AttachmentDov9MMClass {}
+FORTEX_AttachmentDov9MMClass FORTEX_AttachmentDov9MMSource;
+class FORTEX_AttachmentDov9MM : BaseAttachmentType {};
